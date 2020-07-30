aa7014b33c16afc2cc7610f25bb4e7adc9c0cef9
rrodzstgo <ronalderodriguez@gmail.com>
2020-07-30 17:48:31 -0400

    parsed_arguments

--- audio_file_to_text.py
+++ audio_file_to_text.py
@@ -1,19 +1,31 @@
-def audio_file_to_text(input_language,output_name):
+import argparse
 
+parser = argparse.ArgumentParser(description='Transcribe audio file into text')
+# Define the arguments
+parser.add_argument('--input_file', '-i', type=str, dest='input_file', required=True, help='path of the input audio file')
+parser.add_argument('--input_language', '-l', type=str, dest='input_language', required=True,
+                    help='language to use to interpret the audio')
+parser.add_argument('--output_name', '-o', type=str, dest='output_name', required=True, help='path for output text file')
+args = parser.parse_args()
+
+
+def audio_file_to_text(input_file, input_language, output_name):
     if input_language == None:
-    input_language == 'en-US'
+        print("choose a language to be used")
 
     import speech_recognition as sr
 
     r = sr.Recognizer()
 
-#use an audio file as input for the data
-    audio_file = sr.AudioFile('voicemail-23.wav')
+    # use an audio file as input for the data
+    audio_file = sr.AudioFile(str(input_file))
 
     with audio_file as source:
         audio = r.record(source)
-        text = r.recognize_google(audio)
+        text = r.recognize_google(audio, language=str(input_language))
         print(text)
-    audio_file_text_output = open("audio_file_transcript.txt",'w')
+    audio_file_text_output = open(str(output_name) + '.txt', 'w')
     audio_file_text_output.write(text)
     audio_file_text_output.close()
+
+audio_file_to_text(args.input_file, args.input_language, args.output_name)
