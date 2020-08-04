dadaf9ec8d84214913065a7e22ae1a37b0a04505
rrodzstgo <ronalderodriguez@gmail.com>
2020-08-04 17:43:06 -0400

    modified

--- audio_file_to_text.py
+++ audio_file_to_text.py
@@ -1,33 +1,24 @@
 #!/usr/bin/env python
 #establish arguments
 import argparse
+from wit import Wit
 parser = argparse.ArgumentParser(description='Transcribe audio file into text')
 # Define the arguments
 parser.add_argument('--input_file', '-i', type=str, dest='input_file', required=True, help='path of the input audio file')
-parser.add_argument('--input_language', '-l', type=str, dest='input_language', required=False,
-                    help='language to use to interpret the audio')
+#parser.add_argument('--input_language', '-l', type=str, dest='input_language', required=False,
+#                    help='language to use to interpret the audio')
 parser.add_argument('--output_name', '-o', type=str, dest='output_name', required=True, help='path for output text file')
 args = parser.parse_args()
 
 #function to take the audio file and transcribe it using google
-def audio_file_to_text(input_file, input_language, output_name):
-    if input_language == None:
-        print("choose a language to be used")
-
-    import speech_recognition as sr
-
-    r = sr.Recognizer()
-
-    # use an audio file as input for the data
-    audio_file = sr.AudioFile(str(input_file))
-
-    with audio_file as source:
-        audio = r.record(source)
-        text = r.recognize_google(audio, language=str(input_language))
-        print(text)
+def audio_file_to_text(input_file, output_name):
+    client = Wit('G5B355BBFZBLVXKYQSOABXUCB7G5Q6DY')
+    resp = None
+    with open(str(input_file), 'rb') as f:
+        resp = client.speech(f, {'Content-Type': 'audio/wav'})
     audio_file_text_output = open(str(output_name) + '.txt', 'w')
-    audio_file_text_output.write(text)
+    audio_file_text_output.write(str(resp))
     audio_file_text_output.close()
 
 #function with the corresponding arguments
-audio_file_to_text(args.input_file, args.input_language, args.output_name)
+audio_file_to_text(args.input_file, args.output_name)
